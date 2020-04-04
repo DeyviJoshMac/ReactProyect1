5932246a998d088f5253086955cc38f262a97a7f
Deyvi Bustamante Perez <deyvi.adagroup@gmail.com>
2020-04-04 12:13:09 -0400

    añandiendo webpab para gestor de contenido imagens

---
+++ src/containers/App.jsx
@@ -0,0 +1,48 @@
+ import React, {useEffect, useState} from 'react';
+ import Header from '../components/Header';
+ import Search from '../components/Search';
+ import Categories from '../components/Categories';
+ import Carousel from '../components/Carousel';
+ import CarouselItem from '../components/CarouselItem';
+ import Footer from '../components/Footer';
+
+ import '../assets/styles/App.scss';
+ 
+
+ const App = () =>{
+    const[videos, setvideos]=useState([]);
+
+    useEffect(()=> {
+        fetch('http://localhost:3000/initalState')
+        .then(response=> response.json())
+        .then(data=> setvideos(data));
+    }, []);
+
+
+    return (
+        <div className="App">
+            <Header/>
+            <Search/>
+            <Categories title="Mi Lista">
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales Para  Deyvi Bustamante">
+                <Carousel>
+                    <CarouselItem/> 
+                </Carousel>
+            </Categories>
+
+            <Footer/>
+        </div>
+    );
+    }
+ export default App;
