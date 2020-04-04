6571692b63a62e5a3d976f54bea1772b48bfa037
Deyvi Bustamante Perez <deyvi.adagroup@gmail.com>
2020-04-04 16:19:42 -0400

    custome hooks insertado, uso de variables useInitialState y envio de variables

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,37 +5,23 @@
  import Carousel from '../components/Carousel';
  import CarouselItem from '../components/CarouselItem';
  import Footer from '../components/Footer';
+ import useInitialState from '../hooks/useInitialState';
 
  import '../assets/styles/App.scss';
  
+ const API ='http://localhost:3000/initalState';
+
 
  const App = () =>{
-    /*
-     no definimos nuestra variables de paso a la api, y nos salia error
-     //const[videos, setVideos] = useState([]);
-    */
-
-    /*
-        Forma alternativa de verficar
-    */
-    const [ videos , setVideos]=useState({
-        mylist:[],
-        trends: [],
-        originals: []
-    });
     
-    useEffect(()=> {
-        fetch('http://localhost:3000/initalState')
-        .then(response=> response.json())
-        .then(data=> setVideos(data));
-    }, []);
- 
+    const initialState=useInitialState (API);
+    
     return (
         <div className="App">
             <Header/>
             <Search/>
 
-            { videos.mylist.length > 0 && 
+            { initialState.mylist.length > 0 && 
                 <Categories title="Mi Lista">
                     <Carousel>
                         <CarouselItem/>
@@ -44,7 +30,7 @@
             }
             <Categories title="Tendencias">
                 <Carousel>
-                    {videos.trends.map(item=>
+                    {initialState.trends.map(item=>
                         <CarouselItem key={item.id} {...item}/>
                     )}
                 </Carousel>
